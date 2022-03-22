cbbc0601467651ebea7fb48f3cf3a2914237ce43
Gabrie11a <98079657+Gabrie11a@users.noreply.github.com>
2022-03-22 20:11:17 +0800

    Update Utilities.java

--- src/main/java/Tools/Utilities.java
+++ src/main/java/Tools/Utilities.java
@@ -3,22 +3,12 @@ package Tools;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 
-/**
- *
- */
 public class Utilities {
 
 
@@ -49,99 +39,86 @@ public class Utilities {
         return str;
     }
 
-    /**
-     * 写入文件
-     *
-     * @param filePath  要存入的文件路径
-     * @param list JSONObject的list
-     * @throws IOException
-     */
-    public static void writeLocalJson(String filePath,List<JSONObject> list) throws IOException {
+    public static <T> T searchObject(JSONArray array, String key, String value, Class<T> tClass) {
+
+        JSONObject temp = new JSONObject();
+
+        for (int i = 0; i < (array.size()); i++) {
+            Object obj =  array.get(i);
+            JSONObject ob= (JSONObject) obj;
+            if (ob.get(key) == value) {
+                //        temp = ob;
+                //    }
+                if (ob.containsKey(key)) {
+                    temp = ob;
+                    //System.out.println(ob);
+
+                    if (ob.containsValue(value)) {
+                        temp = ob;
+                        //System.out.println(ob);
+                    }
+                }
+
+
+
+            }
+        }
+        return (T) temp.toJavaObject(tClass);
+    }
+
+    public static <T> T json2Bean (JSONObject jsonObject, Class<T> tClass){
+        return (T)jsonObject.toJavaObject(tClass);
+    }
+
+
+    public static void creatFile(String str, String fullPath) throws IOException {
 
         // Creat a file
-        File file = new File(filePath);
-        if (!file.getParentFile().exists())
-        {
-            // Creat parent file if not exists
-            file.getParentFile().mkdirs();
-        }
-        if (file.exists()) {
-            // Delete the file if exists
-            file.delete();
-        }
+        File file = new File(fullPath);
+        if (!file.getParentFile().exists()) {file.getParentFile().mkdirs();}
+        if (file.exists()) {file.delete();}
         file.createNewFile();
-
-        Object json = JSONObject.toJSON(list);
 
         // Write
         Writer write = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
-        write.write(json.toString());
+        write.write(str);
         write.flush();
         write.close();
+
     }
 
-
-    /**
-     * 对某一个value进行修改
-     *
-     * @param str 要改变的JSONString，这里是某个passenger
-     * @param key2Change 要改变的value
-     * @param newValue 改变后的value
-     * @return jsonString
-     */
-    public static String update(String str,String key2Change, String newValue) {
-        JSONObject json = JSONObject.parseObject(str);
-        Set<String> keySet = json.keySet();
-        for(String key:keySet) {
-            Object obj = json.get(key);
-            if(obj instanceof JSONArray) {
-                JSONArray arr = (JSONArray)obj;
-                for(int i=0;i<arr.size();i++) {
-                    String child = update(arr.get(i).toString(),null,null);
-                    arr.set(i, JSONObject.parse(child));
-                }
-                json.put(key, arr);
-            }else if(obj instanceof JSONObject){
-                JSONObject sub = (JSONObject)obj;
-                String substr = update(sub.toJSONString(),null,null);
-                json.put(key, JSONObject.parse(substr));
-            }else {
-                if(key.equals(key2Change)) {
-                    json.put(key, newValue);
-                    //System.out.println("update...");
-                }
-            }
+    public static JSONObject array2Object(JSONArray array){
+        JSONObject temp = new JSONObject();
+        for (int i = 0; i < (array.size()); i++) {
+            temp = array.getJSONObject(i);
         }
-        return json.toJSONString();
+        return temp;
     }
 
-    /**
-     * 通过reference查找
-     * print "surName+iDNO"
-     *
-     * @param array
-     * @param searchName 此处设置为surName
-     * @param reference  此处设置为iDNo
-     */
-    public static void searchPs(JSONArray array,String searchName,String reference) {
+    public static JSONObject fileToJson(JSONObject json,String fileName) {
 
-        for (int i = 0; i < (array.size()); i++)
-        {//循环json数组
+        try (
+                InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+        ) {
+            json = JSONObject.parseObject(IOUtils.toString(is, "utf-8"));
+        } catch (Exception e) {
+            System.out.println(fileName + "Error" + e);
+        }
+        return json;
+    }
 
-            JSONObject ob = (JSONObject) array.get(i);//得到json对象
-            String str = ob.toJSONString();
-            HashMap map = JSON.parseObject(str, HashMap.class);
-            for (Object key : map.keySet()) {
-                Object bb = searchName;
-                if (map.get(key).equals(bb)){
-                    String name1= ob.getString(reference);
-                    System.out.println(map.get(key)+" "+name1);
-                }
+    public static JSONObject operateJsonObject(JSONObject obj,int function,String key,String value){
 
-            }
-
+        JSONObject object=new JSONObject();
+        if(function == 1){
+            obj.put(key,value);
+        } else if (function == 2) {
+            obj.put(key,value);
+        }else if (function == 3){
+            obj.remove(key);
         }
 
+        return obj;
     }
 
 }
