9438221d27f7a69858001e0e25e88023e9cac7fd
Norman-Ou <ouruizhe@bupt.edu.cn>
2022-04-16 23:54:17 +0800

    Updating pom.xml

---
+++ src/main/java/PassengerEnd/MonitorTest.java
@@ -0,0 +1,10 @@
+package PassengerEnd;
+
+public class MonitorTest {
+
+    public static void main(String[] args) {
+        Listeners listeners = new Listeners();
+        listeners.firstOption();
+        listeners.showBookedFlightInfo();
+    }
+}
