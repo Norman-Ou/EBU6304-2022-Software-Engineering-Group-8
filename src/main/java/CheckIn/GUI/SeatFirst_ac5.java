06eabcd4370539bdb816f8e6e94093e95c50e55c
Gabrie11a <98079657+Gabrie11a@users.noreply.github.com>
2022-04-25 23:10:06 +0800

    check in GUI

--- src/main/java/CheckIn/GUI/SeatFirst_ac5.java
+++ src/main/java/CheckIn/GUI/SeatFirst_ac5.java
@@ -27,6 +27,7 @@ public class SeatFirst_ac5 extends JFrame {
     }
 
     private void VIPSeat(ActionEvent e) throws DataNotFound {
+        JOptionPane.showMessageDialog(null, "There would be an additional payment for VIP seats.","Additional payment check", JOptionPane.WARNING_MESSAGE);
         dispose();
         new VIPSeat().setVisible(true);
     }
@@ -48,78 +49,65 @@ public class SeatFirst_ac5 extends JFrame {
     private void initComponents() {
 
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - unknown
+        // Generated using JFormDesigner Evaluation license - Gabirella
         ResourceBundle bundle = ResourceBundle.getBundle("Check");
         dialogPane = new JPanel();
         label1 = new JLabel();
+        panel1 = new JPanel();
         button1 = new JButton();
         button2 = new JButton();
-        scrollPane1 = new JScrollPane();
-        textArea1 = new JTextArea();
 
         //======== this ========
-        Container contentPane = getContentPane();
+        var contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
         //======== dialogPane ========
         {
             dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
             dialogPane.setOpaque(false);
-            dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new
-            javax. swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax
-            . swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java
-            .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt
-            . Color. red) ,dialogPane. getBorder( )) ); dialogPane. addPropertyChangeListener (new java. beans.
-            PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r" .
-            equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+            dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder
+            ( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border
+            . TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt
+            . Color. red) ,dialogPane. getBorder( )) ); dialogPane. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void
+            propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( )
+            ; }} );
             dialogPane.setLayout(new BorderLayout());
 
             //---- label1 ----
             label1.setText(bundle.getString("label1.text_25"));
-            label1.setFont(new Font("Lucida Grande", Font.BOLD, 26));
+            label1.setFont(new Font("Lucida Grande", Font.BOLD, 30));
             label1.setHorizontalAlignment(SwingConstants.CENTER);
             dialogPane.add(label1, BorderLayout.NORTH);
 
-            //---- button1 ----
-            button1.setText(bundle.getString("button1.text_13"));
-            button1.setOpaque(false);
-            button1.addActionListener(e -> {
-                try {
-                    NorSeat(e);
-                } catch (DataNotFound ex) {
-                    ex.printStackTrace();
-                }
-            });
-            dialogPane.add(button1, BorderLayout.WEST);
-
-            //---- button2 ----
-            button2.setText(bundle.getString("button2.text_10"));
-            button2.setOpaque(false);
-            button2.addActionListener(e -> {
-                try {
-                    VIPSeat(e);
-                } catch (DataNotFound ex) {
-                    ex.printStackTrace();
-                }
-            });
-            dialogPane.add(button2, BorderLayout.EAST);
-
-            //======== scrollPane1 ========
+            //======== panel1 ========
             {
-                scrollPane1.setOpaque(false);
+                panel1.setLayout(new GridLayout());
 
-                //---- textArea1 ----
-                textArea1.setText("\n\n\n\n               There would be  \n            an additional payment  \n                 for VIP Seats");
-                textArea1.setLineWrap(true);
-                textArea1.setEditable(false);
-                textArea1.setTabSize(20);
-                textArea1.setWrapStyleWord(true);
-                textArea1.setFont(new Font(Font.MONOSPACED, Font.BOLD, 26));
-                textArea1.setAlignmentX(50.0F);
-                textArea1.setAlignmentY(50.0F);
-                scrollPane1.setViewportView(textArea1);
+                //---- button1 ----
+                button1.setText(bundle.getString("button1.text_13"));
+                button1.setOpaque(false);
+                button1.addActionListener(e -> {
+                    try {
+                        NorSeat(e);
+                    } catch (DataNotFound ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                panel1.add(button1);
+
+                //---- button2 ----
+                button2.setText(bundle.getString("button2.text_10"));
+                button2.setOpaque(false);
+                button2.addActionListener(e -> {
+                    try {
+                        VIPSeat(e);
+                    } catch (DataNotFound ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                panel1.add(button2);
             }
-            dialogPane.add(scrollPane1, BorderLayout.CENTER);
+            dialogPane.add(panel1, BorderLayout.CENTER);
         }
         contentPane.add(dialogPane, BorderLayout.CENTER);
         setSize(900, 550);
@@ -130,12 +118,11 @@ public class SeatFirst_ac5 extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - unknown
+    // Generated using JFormDesigner Evaluation license - Gabirella
     private JPanel dialogPane;
     private JLabel label1;
+    private JPanel panel1;
     private JButton button1;
     private JButton button2;
-    private JScrollPane scrollPane1;
-    private JTextArea textArea1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
