a001afbae8b1aa448bf03d10314f420c84355d3f
Gabire11a <songsedrama@gmail.com>
2022-04-29 13:47:15 +0800

    Remove baggage payment

--- src/main/java/CheckIn/GUI/Seat1.java
+++ src/main/java/CheckIn/GUI/Seat1.java
@@ -138,11 +138,11 @@ public class Seat1 extends JFrame {
         {
             panel3.setOpaque(false);
             panel3.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing
-            . border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax. swing .border . TitledBorder
-            . CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "D\u0069alog", java .
+            . border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing .border . TitledBorder
+            . CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "D\u0069al\u006fg", java .
             awt . Font. BOLD ,12 ) ,java . awt. Color .red ) ,panel3. getBorder () ) )
             ; panel3. addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
-            ) { if( "\u0062order" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } )
+            ) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } )
             ;
             panel3.setLayout(new FlowLayout());
 
