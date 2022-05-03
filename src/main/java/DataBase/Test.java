a2dcc36a13d5616c500d3bb0f04d74268b6439f5
Norman-Ou <ouruizhe@bupt.edu.cn>
2022-05-03 13:21:18 +0800

    MOVE python folder out of src

--- src/main/java/DataBase/Test.java
+++ src/main/java/DataBase/Test.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args) {
         try {
-            ArrayList<Passenger> arrayList = pDB.loadPassengersBySurname_ID("Cadashian", "243565");
+            ArrayList<Passenger> arrayList = pDB.loadPassengersBySurname_ID("Harris", "619531");
             System.out.println(arrayList.toString());
         } catch (DataNotFound dataNotFound) {
             dataNotFound.printStackTrace();
