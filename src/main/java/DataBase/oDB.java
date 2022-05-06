54518f708d0759df455e98061b035dcb0951c273
Norman-Ou <ouruizhe@bupt.edu.cn>
2022-05-06 17:10:04 +0800

    add getOrdersByFlightNo and getOrdersByPassengerId method in oDB

--- src/main/java/DataBase/oDB.java
+++ src/main/java/DataBase/oDB.java
@@ -1,10 +1,14 @@
 package DataBase;
 
 import Beans.Order.Order;
+import Beans.Passenger.Passenger;
 import Config.Config;
 import Exceptions.DataNotFound;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class oDB {
 
@@ -29,6 +33,52 @@ public class oDB {
             return null;
         } catch (DataNotFound dataNotFound) {
             throw new DataNotFound(bookNumber + " Order Not Found");
+        }
+    }
+
+    public static ArrayList<Order> getOrdersByFlightNo(String flightNo) throws DataNotFound{
+        DataBase dataBase = new DataBase(Config.OrderFile);
+        ArrayList<Order> arrayList = new ArrayList<>();
+
+        try {
+            JSONArray array = dataBase.getAllObject();
+            for (int i = 0; i < (array.size()); i++) {
+                JSONObject ob = (JSONObject) array.get(i);
+                if (ob.get("flightNo").equals(flightNo)) {
+                    arrayList.add(ob.toJavaObject(Order.class));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (arrayList.isEmpty()){
+            throw new DataNotFound("DataNotFound");
+        } else {
+            return arrayList;
+        }
+    }
+
+    public static ArrayList<Order> getOrdersByPassengerId(String passengerId) throws DataNotFound{
+        DataBase dataBase = new DataBase(Config.OrderFile);
+        ArrayList<Order> arrayList = new ArrayList<>();
+
+        try {
+            JSONArray array = dataBase.getAllObject();
+            for (int i = 0; i < (array.size()); i++) {
+                JSONObject ob = (JSONObject) array.get(i);
+                if (ob.get("passengerId").equals(passengerId)) {
+                    arrayList.add(ob.toJavaObject(Order.class));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (arrayList.isEmpty()){
+            throw new DataNotFound("DataNotFound");
+        } else {
+            return arrayList;
         }
     }
 }
