0400c5dfb3274052c67c1a4e2e20294fc0c0a01d
Norman-Ou <ouruizhe@bupt.edu.cn>
2022-05-22 14:56:31 +0800

    Comment in Config.java

--- src/main/java/Config/Config.java
+++ src/main/java/Config/Config.java
@@ -2,7 +2,8 @@ package Config;
 
 
 public class Config {
-    public static final String PassengerFile = "src/main/resources/passenger_data_v4.json";
+    //------2020/05/22 data version v6------//
+    public static final String PassengerFile = "src/main/resources/passenger_data_v6.json";
     public static final String FlightFile = "src/main/resources/flight_data_v6.json";
     public static final String OrderFile = "src/main/resources/order_data_v6.json";
     public static final String IDFile = "src/main/resources/id_document.json";
